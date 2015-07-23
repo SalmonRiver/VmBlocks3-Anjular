7c503bb43cee99f3d4fa74ca33962928025dc495
david-nelson-moscow <david.nelson.moscow@gmail.com>
2015-07-23 14:19:29 -0700

    David 2:19

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,23 +11,6 @@
 angular.module('vmBlocks3App')
   .controller('MainCtrl', function ($scope, $http, $location, VmWebAPI) {
     // dummies values before we hook up to  a web service
-<<<<<<< HEAD
-   
-    var VmesaURL = "http://demo1.svmesa.com/vmwebapi/Odata/MesaBlocks";
-
-    $http.get(VmesaURL)
-      .success(function (response) {
-        // console.log (response);
-        $scope.RawHtmlReturn = response;
-        $scope.blocks = response.value
-         console.log (response.value);
-      });
-      
-      $scope.NavigateTo = function() {
-        console.log("calling navigate " + $scope.selectedBlock.sName);   
-        
-        $location.path("/MesaBlock/" +  $scope.selectedBlock.sName);     // need to add the block to load the form on 
-=======
     
     var OnComplete = function (data) {
       $scope.blocks = data;
@@ -40,8 +23,7 @@ angular.module('vmBlocks3App')
     $scope.NavigateTo = function () {
       console.log("calling navigate " + $scope.selectedBlock);
 
-      $location.path("/MesaBlock");     // need to add the block to load the form on 
->>>>>>> 39e3a3ab40a889084f5000031b5b77c4194b3136
+      $location.path("/MesaBlock/" + $scope.selectedBlock );     // need to add the block to load the form on 
         
     }
 
@@ -50,5 +32,4 @@ angular.module('vmBlocks3App')
 
 
   });
-  
   
